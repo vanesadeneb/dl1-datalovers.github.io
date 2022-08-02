d3c20288e40c0933f6c98e90241446c2424572ce
vanesadeneb <vanessa_diesver@hotmail.com>
2022-08-02 16:27:23 -0700

    update project code

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/pokemon/pokemon.js';
+import data from "./data/rickandmorty/rickandmorty.js";
 
-console.log(example, data);
+for (let i = 0; i < data.results.length; i++) {
+  let figure = document.createElement("figure");
+  let img = document.createElement("img");
+  img.src = data.results[i].image;
+  let figcaption = document.createElement("figcaption");
+
+  let cardFigure = document.querySelector("main").appendChild(figure);
+  cardFigure.appendChild(img);
+  let caption = cardFigure.appendChild(figcaption);
+  let p = caption.appendChild(document.createElement("p"));
+  p.innerHTML = data.results[i].name;
+  //seted a class to change the name color and size in the card
+  p.setAttribute("class", "character-name");
+  let information = caption.appendChild(document.createElement("div"));
+  information.setAttribute("class", "information");
+  //Here we need to add the circle with diferent color
+  let status = information.appendChild(document.createElement("div"));
+  status.setAttribute("class", "status");
+
+  if (data.results[i].status == "Alive") {
+    status.style.borderColor = "green";
+    status.style.backgroundColor = "green";
+  } else if (data.results[i].status == "Dead") {
+    status.style.borderColor = "red";
+    status.style.backgroundColor = "red";
+  } else {
+    status.style.borderColor = "gray";
+    status.style.backgroundColor = "gray";
+  }
+
+  let aliveSpan = information.appendChild(document.createElement("span"));
+  aliveSpan.innerHTML = data.results[i].status + " - ";
+  let specieSpan = information.appendChild(document.createElement("span"));
+  specieSpan.innerHTML = data.results[i].species + " - ";
+  let genderSpan = information.appendChild(document.createElement("span"));
+  genderSpan.innerHTML = data.results[i].gender;
+}
